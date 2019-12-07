cfff731957b5819c342ba98150fdeb8d0c1f52e4
Asier Bilbao <abilbaotm@gmail.com>
2019-12-07 16:47:12 +0100

    añadir @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,14 @@
  */
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyBCYfRAt78hMiH0z3Bow0OU5oSfPBBCAmE',
+    authDomain: 'dw-bote.firebaseapp.com',
+    databaseURL: 'https://dw-bote.firebaseio.com',
+    projectId: 'dw-bote',
+    storageBucket: 'dw-bote.appspot.com',
+    messagingSenderId: '53136915015',
+    appId: '1:53136915015:web:04ecdbdb6a53e212020f62',
+    measurementId: 'G-PKD6CKZGBE',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,4 +10,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyBCYfRAt78hMiH0z3Bow0OU5oSfPBBCAmE',
+    authDomain: 'dw-bote.firebaseapp.com',
+    databaseURL: 'https://dw-bote.firebaseio.com',
+    projectId: 'dw-bote',
+    storageBucket: 'dw-bote.appspot.com',
+    messagingSenderId: '53136915015',
+    appId: '1:53136915015:web:04ecdbdb6a53e212020f62',
+    measurementId: 'G-PKD6CKZGBE',
+  },
 };
