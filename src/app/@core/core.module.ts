da008b7b6f7cc67805a62d76ecd7922ba61c7fec
Asier Bilbao <abilbaotm@gmail.com>
2019-12-09 15:28:32 +0100

    remove NbDummyAuthStrategy

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NbAuthModule, NbDummyAuthStrategy, NbPasswordAuthStrategy} from '@nebular/auth';
+import {NbAuthModule, NbPasswordAuthStrategy} from '@nebular/auth';
 import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
 import { of as observableOf } from 'rxjs';
 
