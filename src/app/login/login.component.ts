1ef0d0fa6e45b4c83b2ce968f22b0125de79868a
Asier Bilbao <abilbaotm@gmail.com>
2019-12-17 10:38:14 +0100

    arreglar redirects

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -33,14 +33,14 @@ export class LoginComponent {
   tryGoogleLogin(){
     this.authService.doGoogleLogin()
       .then(res => {
-        this.router.navigate(['/user']);
+        this.router.navigate(['/dashboard']);
       })
   }
 
   tryLogin(value){
     this.authService.doLogin(value)
       .then(res => {
-        this.router.navigate(['/user']);
+        this.router.navigate(['/dashboard']);
       }, err => {
         console.log(err);
         this.errorMessage = err.message;
