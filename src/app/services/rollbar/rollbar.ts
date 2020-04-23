7ba8d8a67d30d42ceb64fc4fb488f5d830956257
Asier Bilbao <abilbaotm@gmail.com>
2020-04-23 17:39:02 +0200

    define codeVersion

--- src/app/services/rollbar/rollbar.ts
+++ src/app/services/rollbar/rollbar.ts
@@ -3,11 +3,13 @@ import Rollbar from 'rollbar';
 
 import {ErrorHandler, Inject, Injectable, InjectionToken} from '@angular/core';
 import {environment} from "../../../environments/environment";
+import {environmentversion} from "../../../environments/environmentversion";
 
-const rollbarConfig = {
+const rollbarConfig: Rollbar.Configuration = {
   accessToken: environment.ROLLBAR_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  codeVersion: environmentversion
 };
 
 export const RollbarService = new InjectionToken<Rollbar>('rollbar');
